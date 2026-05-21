470b276fa042d3e149aea6163d1967e57fabc8c9
Raaid-Shaheer <sahlraaid52@gmail.com>
2026-05-21 23:27:30 +0530

    prepare to host in render

--- backend/database.py
+++ backend/database.py
@@ -5,22 +5,26 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-# Force SQLAlchemy to use the pymysql driver
-if DATABASE_URL and DATABASE_URL.startswith("mysql://"):
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////data/skillforge.db")
+
+if DATABASE_URL.startswith("mysql://"):
     DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+pymysql://", 1)
 
-# If no URL is found (local testing), fallback to sqlite
-engine = create_engine(DATABASE_URL,pool_pre_ping=True)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 def get_db():
-  db = SessionLocal()
-  try:
-    yield db
-  finally:
-    db.close()
-    
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
